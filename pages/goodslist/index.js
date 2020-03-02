6025b8d22300a24e5db9e212d100eace62c17b37
Donald <1376825153@qq.com>
2020-03-02 18:24:38 +0800

    实现商品列表页的布局

---
+++ pages/goodslist/index.js
@@ -0,0 +1,35 @@
+import request from '../../utils/request.js';
+
+Page({
+
+  data:{
+    tab:['综合','销量','价格'],
+    current:0,
+    keyword:'曲面电视'
+  },
+
+  tabClick(e){
+    const {index}=e.currentTarget.dataset
+    this.setData({
+      current:index
+    })
+  },
+
+  onLoad:function(options){
+    this.setData({
+      keyword:options.keyword
+    })
+
+    request({
+      url:'/goods/search',
+      data:{
+        query:this.data.keyword,
+        pagenum:1,
+        pagesize:10
+      }
+    }).then(res=>{
+      console.log(res)
+    })
+
+  }
+})
