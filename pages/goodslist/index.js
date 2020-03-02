94f57d5dc6fd9eaef4e9b1741ec2c13d74883038
Donald <1376825153@qq.com>
2020-03-02 21:37:15 +0800

    完成上拉加载功能

--- pages/goodslist/index.js
+++ pages/goodslist/index.js
@@ -6,7 +6,10 @@ Page({
     tab:['综合','销量','价格'],
     current:0,
     keyword:'曲面电视',
+    pagenum:1,
     goods:[],
+    hasMore:true,
+    loading:true
   },
 
   tabClick(e){
@@ -17,23 +20,47 @@ Page({
   },
 
   onLoad:function(options){
-    // this.setData({
-    //   keyword:options.keyword
-    // })
-
-    request({
-      url:'/goods/search',
-      data:{
-        query:this.data.keyword,
-        pagenum:1,
-        pagesize:10
-      }
-    }).then(res=>{
-      console.log(res)
-      this.setData({
-        goods:res.data.message.goods
-      })
+    this.setData({
+      keyword:options.keyword
     })
+    this.getData()
 
+  },
+
+  getData(){
+    if(this.data.hasMore==false){
+      return
+    }
+    request({
+      url: '/goods/search',
+      data: {
+        query: this.data.keyword,
+        pagenum: this.data.pagenum,
+        pagesize: 10
+      }
+    }).then(res => {
+      console.log(res)
+      const {message}=res.data;
+      const goods=message.goods.map(v=>{
+        v.goods_price=Number(v.goods_price).toFixed(2)
+        return v
+      })
+      this.setData({
+        goods:[...this.data.goods,...goods]
+      })
+      if(this.data.goods.length>=message.total){
+        this.setData({
+          hasMore:false
+        })
+      }
+      
+    })
+  },
+
+  onReachBottom(){
+    this.setData({
+      pagenum:this.data.pagenum+1
+    })
+    this.getData()
   }
 })
