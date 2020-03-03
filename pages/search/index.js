32c903b5afe60ab7e1d00593eb528c24ba657931
Donald <1376825153@qq.com>
2020-03-03 18:15:20 +0800

    请求数据展示搜索结果

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,30 @@
-// pages/search/index.js
+import request from '../../utils/request.js';
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  data:{
+    inputvalue:'',
+    recommendlist:[],
 
-  },
+  } ,
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  handleinput(e){
+    const {value}=e.detail;
+    this.setData({
+      inputvalue:value
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    request({
+      url:'/goods/qsearch',
+      data:{
+        query:this.data.inputvalue
+      }
+    }).then(res=>{
+      console.log(res)
+      const {message}=res.data;
+      this.setData({
+        recommendlist:message
+      })
+    })
   }
 })
