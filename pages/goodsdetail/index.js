b5dbbea80c8b932d583697906f6b0297df6eb4bb
Donald <1376825153@qq.com>
2020-03-06 22:00:53 +0800

    实现商品添加到购物车功能

--- pages/goodsdetail/index.js
+++ pages/goodsdetail/index.js
@@ -44,5 +44,44 @@ Page({
     wx.switchTab({
       url: '/pages/cart/index'
     })
+  },
+
+  addgoods(){
+    const goods=wx.getStorageSync('goods')||[];
+    const exit=goods.some(v=>{
+      const isExit = v.id === this.data.goodsmessage.goods_id;
+      if(isExit){
+        v.number+=1;
+        wx.showToast({
+          title: '数量+1',
+          icon: 'success'
+        })
+      }
+      return isExit
+    })
+
+    if(!exit){
+      goods.unshift({
+        image: this.data.goodsmessage.goods_small_logo,
+        name: this.data.goodsmessage.goods_name,
+        price: this.data.goodsmessage.goods_price,
+        id: this.data.goodsmessage.goods_id,
+        number:1
+      })
+      wx.showToast({
+        title: '加入成功',
+        icon: 'success'
+      })
+    }
+    wx.setStorageSync('goods', goods)
+  },
+
+  addcargo(){
+    this.addgoods();
+    setTimeout(v=>{
+      wx.switchTab({
+        url: '/pages/cart/index'
+      })
+    },1000)
   }
 })
